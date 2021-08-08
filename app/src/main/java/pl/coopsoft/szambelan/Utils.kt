be2b6b587a7aee04b4e29e00bfebdfa16c43ccbb
Adam Dybkowski <adybkows@gmail.com>
2021-08-09 01:44:01 +0200

    Accept commas and dots in number fields. Allow scrolling when keyboard is shown. Update dependencies.

--- app/src/main/java/pl/coopsoft/szambelan/Utils.kt
+++ app/src/main/java/pl/coopsoft/szambelan/Utils.kt
@@ -1,17 +1,11 @@
 package pl.coopsoft.szambelan
 
 import android.content.Context
-import java.text.NumberFormat
-import java.text.ParseException
 import java.util.*
 
 object Utils {
     fun toDouble(s: String) =
-        try {
-            NumberFormat.getInstance().parse(s)?.toDouble() ?: 0.0
-        } catch (e: ParseException) {
-            0.0
-        }
+        s.replace(',', '.').toDoubleOrNull() ?: 0.0
 
     fun toString(d: Double) =
         String.format(Locale.getDefault(), "%1$.2f", d)
